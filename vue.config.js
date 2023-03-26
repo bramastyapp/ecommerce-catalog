1ae4fd9b0e22bec7cf7ae735b2d07a9f50adc7a6
bramastyapp <bramastya7@gmail.com>
2023-03-27 03:11:36 +0700

    update config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  base: "/ecommerce-catalog/",
 });
+module.exports = {
+  publicPath: "/ecommerce-catalog/",
+};
